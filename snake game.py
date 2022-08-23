3d0b06dacfa73d13764e730ed1632b6a91bb7386
KWagnerCS <111701655+KWagnerCS@users.noreply.github.com>
2022-08-23 17:43:01 -0400

    step 1  ok

---
+++ snake game.py
@@ -0,0 +1,105 @@
+
+import math
+import random
+from re import L
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx = 1, dirny = 0, color = (255,0,0)) -> None:
+        pass
+    
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes = False):
+        pass
+
+    
+class snake(object):
+    body = []
+    turns = {}
+
+    def __init__(self, color, pos) -> None:
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                if keys[pygame.K_RIGHT]:
+
+                    
+                if keys[pygame.K_UP]:
+
+
+                if keys[pygame.K_DOWN]:
+                    
+
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (0, 0, 0), (x, 0), (x, w))
+        pygame.draw.line(surface, (0, 0, 0), (0, y), (w, y))
+    
+
+def redrawWindow(surface):
+    global rows, width
+    surface.fill((255, 255, 255))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+    
+
+def randomSnack(rows, itmes):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    global width, rows
+    width = 500
+    rows = 10
+    win = pygame.display.set_mode((width, width))
+    s = snake((255, 0, 0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
+
+main()
